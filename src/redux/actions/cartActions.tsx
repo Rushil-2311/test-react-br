9ae73e80102ac935ba495dc426ded908eef92ec7
Rushil-2311 <rushilpatellinux2gmail.com>
2022-09-19 21:20:49 +0530

    removed-non-functional-code

--- src/redux/actions/cartActions.tsx
+++ src/redux/actions/cartActions.tsx
@@ -9,7 +9,6 @@ import {
 
 export const getCartProductAction: any =
   (values: ICart[]) => async (dispatch: AppDispatch) => {
-    // Api call for register
     const response = values;
     if (response) {
       dispatch(getCartProducts(response));
@@ -18,26 +17,14 @@ export const getCartProductAction: any =
 
 export const addCartProductAction: any =
   (values: ICart) => async (dispatch: AppDispatch) => {
-    // Api call for Add user
-
     const response = values;
     if (response) {
       dispatch(addCartProduct(response));
     }
   };
 
-export const changeQuantityOfProductAction: any =
-  (values: string) => async (dispatch: AppDispatch) => {
-    // Api call for delete user
-    const response = values;
-    if (response) {
-      dispatch(changeQuantityProduct(response));
-    }
-  };
-
 export const deleteCartProductAction: any =
   (values: string) => async (dispatch: AppDispatch) => {
-    // Api call for delete user
     const response = values;
     if (response) {
       dispatch(deleteCartProduct(response));
--- src/redux/actions/index.tsx
+++ src/redux/actions/index.tsx
@@ -4,27 +4,8 @@ import { IProduct } from "../interface/product";
 
 export const getProductAction: any =
   (values: IProduct[]) => async (dispatch: AppDispatch) => {
-    // Api call for register
     const response = values;
     if (response) {
       dispatch(getProducts(response));
-    }
-  };
-
-export const addProductAction: any =
-  (values: IProduct) => async (dispatch: AppDispatch) => {
-    // Api call for Add user
-    const response = values;
-    if (response) {
-      dispatch(addProduct(response));
-    }
-  };
-
-export const deleteProductAction: any =
-  (values: string) => async (dispatch: AppDispatch) => {
-    // Api call for delete user
-    const response = values;
-    if (response) {
-      dispatch(deleteProduct(response));
     }
   };
